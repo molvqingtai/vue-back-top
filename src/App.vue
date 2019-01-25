99df2cfdb50c6f4e6e88961510538bcdff5748ff
John Wu <molvqingtai@gmail.com>
2019-01-25 23:53:11 +0800

    :construction:

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+      <main>
+          <section v-for="n in 5" :key="n"></section>
+      </main>
       <back-top></back-top>
   </div>
 </template>
@@ -10,3 +13,32 @@ export default {
   name: 'app'
 }
 </script>
+
+<style lang="scss" scoped>
+  body{
+    height: 500vh;
+    section{
+      width:1200px;
+      min-width: 1200px;
+      margin: 0 auto;
+      height: 100vh;
+      opacity: .5;
+      &:nth-child(1){
+        background-color: #409EFF;
+      }
+      &:nth-child(2){
+        background-color: #67C23A;
+      }
+      &:nth-child(3){
+        background-color: #E6A23C;
+      }
+      &:nth-child(4){
+        background-color: #F56C6C;
+      }
+      &:nth-child(5){
+        background-color: #909399;
+      }
+    }
+  }
+
+</style>
--- src/components/Back-Top.vue
+++ src/components/Back-Top.vue
@@ -1,14 +1,75 @@
 <template>
-<div class="back-top">
-  <slot>
-    <i class="back-top__icon iconfont">&#xe69e;</i>
-  </slot>
-</div>
+<transition enter-active-class="bounceIn" leave-active-class="bounceOut">
+  <div class="back-top" :style="{right,bottom}" @click="backTop" v-show="show">
+    <slot>
+      <i class="back-top__icon iconfont" :style="{transform:`scale(${size},${size})`,backgroundColor:color}">&#xe69e;</i>
+    </slot>
+  </div>
+</transition>
 </template>
 
 <script>
 export default {
-  name: 'BackTop'
+  name: 'BackTop',
+  props: {
+    'right': {
+      type: String,
+      default: '3%'
+    },
+    'bottom': {
+      type: String,
+      default: '10%'
+    },
+    'color': {
+      type: String,
+      default: '#409EFF'
+    },
+    'size': {
+      type: Number,
+      default: 1
+    },
+    'slow': {
+      type: Number,
+      default: 10
+    }
+  },
+  data () {
+    return {
+      show: false,
+      closure: null
+    }
+  },
+  methods: {
+    backTop () {
+      let doc = document.body.scrollTop ? document.body : document.documentElement
+      let scrollTop = doc.scrollTop
+      let scroll = () => {
+        scrollTop = scrollTop + (0 - scrollTop) / this.slow
+        if (scrollTop < 1) {
+          doc.scrollTop = 0
+        } else {
+          doc.scrollTop = scrollTop
+          requestAnimationFrame(scroll)
+        }
+      }
+      scroll()
+    },
+    listenerScroll () {
+      this.show = window.innerHeight < window.scrollY
+    }
+  },
+  mounted () {
+    console.log(1)
+    window.addEventListener('scroll', this.listenerScroll)
+  },
+  activated () {
+    console.log(2)
+    window.removeEventListener('scroll', this.listenerScroll)
+    window.addEventListener('scroll', this.listenerScroll)
+  },
+  deactivated () {
+    window.removeEventListener('scroll', this.listenerScroll)
+  }
 }
 </script>
 
@@ -16,17 +77,86 @@ export default {
 @import '~@/assets/iconfont.css';
 .back-top {
     position: fixed;
-    right: 5%;
-    bottom: 10%;
     &__icon {
         display: flex;
-        align-items: center;
-        justify-content: center;
         width: 35px;
         height: 35px;
+        align-items: center;
+        justify-content: center;
         border-radius: 50%;
-        background-color: #FFFFFF;
+        color: #FFFFFF;
         box-shadow: 0 0 5px 0 rgba(0,0,0,.3);
+        cursor: pointer;
+        user-select: none;
+        &:active {
+            opacity: 0.9;
+            box-shadow: 0 0 5px 0 rgba(0,0,0,.1);
+        }
+    }
+}
+
+/* animate.css */
+
+.bounceIn {
+    animation-duration: 0.75s;
+    animation-name: bounceIn;
+}
+.bounceOut {
+    animation-duration: 0.75s;
+    animation-name: bounceOut;
+}
+@keyframes bounceIn {
+    20%,
+    40%,
+    60%,
+    80%,
+    from,
+    to {
+        animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+    }
+
+    0% {
+        opacity: 0;
+        transform: scale3d(0.3, 0.3, 0.3);
+    }
+
+    20% {
+        transform: scale3d(1.1, 1.1, 1.1);
+    }
+
+    40% {
+        transform: scale3d(0.9, 0.9, 0.9);
+    }
+
+    60% {
+        opacity: 1;
+        transform: scale3d(1.03, 1.03, 1.03);
+    }
+
+    80% {
+        transform: scale3d(0.97, 0.97, 0.97);
+    }
+
+    to {
+        opacity: 1;
+        transform: scale3d(1, 1, 1);
+    }
+}
+
+@keyframes bounceOut {
+    20% {
+        transform: scale3d(0.9, 0.9, 0.9);
+    }
+
+    50%,
+    55% {
+        opacity: 1;
+        transform: scale3d(1.1, 1.1, 1.1);
+    }
+
+    to {
+        opacity: 0;
+        transform: scale3d(0.3, 0.3, 0.3);
     }
 }
 </style>
